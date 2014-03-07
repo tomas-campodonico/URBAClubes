551d2fc5ac44f87d1055dd41ad7218a9a44bdafb
Tomas Campodonico <tomycsl@gmail.com>
2014-03-07 16:33:44 -0300

    Concatename and minimize css

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,8 +8,18 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       dist: {
-        src: 'js/<%= pkg.name %>.js',
+        src: 'tmp/<%= pkg.name %>.js',
         dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['js/*.js'],
+        dest: 'tmp/<%= pkg.name %>.js'
       }
     },
 
@@ -22,15 +32,25 @@ module.exports = function(grunt) {
           'dist/urba-pc.png': 'img/urba-pc.png'
         }
       }
+    },
+
+    cssmin: {
+      combine: {
+        files: {
+          'dist/main.min.css': ['css/*.css']
+        }
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task(s).
   grunt.registerTask('imagemin', ['imagemin']);
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('build', ['concat', 'uglify', 'cssmin']);
 
 };
