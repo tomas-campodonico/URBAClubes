81c93bde0a3ea77080ecf2166583f25a10298925
Tomas Campodonico <tomas.campodonico@globant.com>
2014-12-12 00:04:53 -0300

    Add map and modal

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,27 @@
 'use strict';
 
 $(document).ready(function() {
+	var mapOpts = {
+    zoom: 9,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    center: new google.maps.LatLng(-34.840013, -58.265991)
+  },
+  	directionsService = new google.maps.DirectionsService(),
+  	directionsDisplay = new google.maps.DirectionsRenderer(),
+  	map = new google.maps.Map($('#map-canvas')[0], mapOpts),
+  	dfd;
 
-	//Read JSON and create list
-	$.getJSON('/clubes.json', function(clubes) {
-	  $.each(clubes, function(key, club) {
-	  	$('.clubList').append('<li data-name="' + club.name +
-	  		'" data-lat="' + club.lat +
-	  		'" data-lon="' + club.lon + '">' + club.shortName + '</li>');
-	  });
-	  $('.clubList li').on('click', rippleEffect);
-	});
+	/* -------------- FUNCTIONS -------------------- */
 
 	//Show or hide the menu container
 	function toggleMenu() {
-		$('aside').toggleClass('opened');
-		$('.searchBox input').val('');
+		if ($('.instructionsModal').hasClass('opened')) {
+			$('.instructionsModal').removeClass('opened');
+			$('.instructionsModal').addClass('hidden');
+		} else {
+			$('aside').toggleClass('opened');
+			$('.searchBox input').val('');
+		}
 	}
 
 	//Show ripple effect
@@ -23,7 +29,7 @@ $(document).ready(function() {
 		var ink, d, x, y;
 
 		//create .ink element if it doesn't exist
-		if($(this).find('.ink').length === 0) {
+		if ($(this).find('.ink').length === 0) {
 			$(this).prepend('<span class="ink"></span>');
 		}
 			
@@ -44,11 +50,53 @@ $(document).ready(function() {
 			top: y + 'px',
 			left: x + 'px'
 		}).addClass('animate');
-	
 	}
 
+	//Get user location
+	function getCurrentPositionDeferred(options) {
+    var deferred = $.Deferred();
+    navigator.geolocation.getCurrentPosition(deferred.resolve, deferred.reject, options);
+    return deferred.promise();
+  }
+
+  //Show route in map
+	function showRouteInMap(request) {
+    directionsService.route(request, function(result, status) {
+      if (status === google.maps.DirectionsStatus.OK) {
+        //_showResults(result);
+        directionsDisplay.setDirections(result);
+        $('.instructionsButton').removeClass('hidden');
+      }
+    });
+  }
+
+	//Calculate route
+	function calcRoute(club) {
+		if (navigator.geolocation) {
+      $.when(dfd).done(function(pos) {
+      	console.dir(pos);
+        showRouteInMap({
+          origin: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+          destination: new google.maps.LatLng(club.lat, club.lon),
+          travelMode: google.maps.TravelMode.DRIVING
+        });
+      });
+    } else {
+    	$('.noGeolocationAlert').removeClass('hidden');
+    	setTimeout(function() {
+    		$('.noGeolocationAlert').addClass('hidden');
+    	}, 6000);
+    }
+  }
+
+  /* -------------------------- HANDLERS ------------------------------- */
 	$('.toggleMenuButton').on('click', toggleMenu);
 	$('.backshadow').on('click', toggleMenu);
+	$('.instructionsButton').on('click', function(ev) {
+		rippleEffect.call(ev.currentTarget, ev);
+		$('.instructionsModal').removeClass('hidden');
+		$('.instructionsModal').addClass('opened');
+	});
 	$('.searchBox input').on('keyup', function() {
 		var searchText = $('.searchBox input').val().toLowerCase();
 
@@ -59,6 +107,35 @@ $(document).ready(function() {
 				$(item).addClass('hidden');
 			}
 		});
+	});
+
+
+	/* -------------------------- INIT ------------------------------------ */
+	//init map  
+  directionsDisplay.setMap(map);
+  directionsDisplay.setPanel($('.instructionsModal .instructions')[0]);
+
+  if (navigator.geolocation) {
+    dfd = getCurrentPositionDeferred();
+  }
+
+	//Read JSON and create list
+	$.getJSON('/clubes.json', function(clubes) {
+	  $.each(clubes, function(key, club) {
+	  	$('.clubList').append('<li data-name="' + club.name +
+	  		'" data-lat="' + club.lat +
+	  		'" data-lon="' + club.lon + '">' + club.shortName + '</li>');
+	  });
+
+	  $('.clubList li').on('click', function(ev) {
+	  	rippleEffect.call(ev.currentTarget, ev);
+	  	toggleMenu();
+	  	calcRoute({
+	  		name: $(ev.currentTarget).text(),
+	  		lat: ev.currentTarget.dataset.lat,
+	  		lon: ev.currentTarget.dataset.lon
+	  	});
+	  });
 	});
 	
 });
